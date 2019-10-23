68f2ad8aad043978ad3e7a558a9b759c95a1db27
AdrianVasquez1 <Pevaev11>
2019-10-23 08:47:45 -0500

    3rd commit for the MonteCarlo program

--- FunWithMath.py
+++ FunWithMath.py
@@ -1,16 +1,17 @@
 # Starting off
-print(22/7)
-print(355/113)
+print(22 / 7)
+print(355 / 113)
 import math
 
 print(9801 / (2206 * math.sqrt(2)))
+
 
 def archimedes(numSides):
     innerAngleB = 360.0 / numSides
     halfAngleA = innerAngleB / 2
     oneHalfSideS = math.sin(math.radians(halfAngleA))
     sideS = oneHalfSideS * 2
-    polgonCircumference  =  numSides * sideS
+    polgonCircumference = numSides * sideS
     pi = polgonCircumference / 2
     return pi
 
@@ -18,9 +19,8 @@ def archimedes(numSides):
 print(archimedes(8))
 print(archimedes(16))
 
-for sides in range (8, 5020000, 48):
+for sides in range(8, 5020000, 48):
     print(sides, archimedes(sides))
-
 
 # experiment with the loop above alongside the actual value of Pi. How many
 # sides does it take to make the two close?
@@ -38,8 +38,8 @@ print(acc)
 maximum = int(input("Please enter maximum value : "))
 total = 0
 
-for number in range(2, maximum+1):
-    if(number % 2 == 0):
+for number in range(2, maximum + 1):
+    if (number % 2 == 0):
         print("{0}".format(number))
         total = total + number
 
@@ -48,8 +48,8 @@ for number in range(2, maximum+1):
 maximum = int(input(" Please Enter the Maximum Value : "))
 Oddtotal = 0
 
-for number in range(1, maximum+1):
-    if(number % 2 != 0):
+for number in range(1, maximum + 1):
+    if (number % 2 != 0):
         print("{0}".format(number))
         Oddtotal = Oddtotal + number
 
@@ -65,29 +65,35 @@ for i in range(1, 200, 2):
 print(total / length)
 # Write a function that returns the average of the first N numbers, where
 #   N is a parameter
-print ("calculate an average of first n natural numbers")
+print("calculate an average of first n natural numbers")
 n = input("Enter Number ")
-n = int (n)
+n = int(n)
 average = 0
 sum = 0
 
-for num in range(0,n+1,1):
-    sum = sum+num;
+for num in range(0, n + 1, 1):
+    sum = sum + num;
 average = sum / n
 print("Average of first ", n, "number is: ", average)
+
+
 # Write a function called factorial that computes the product of the first N
 #   numbers, where N is a parameter
 def factorial(n):
     if n == 0:
         return 1
     else:
-        return n * factorial(n-1)
-n=int(input("Input a number to compute the factiorial : "))
+        return n * factorial(n - 1)
+
+
+n = int(input("Input a number to compute the factiorial : "))
 print(factorial(n))
 # Each number in the Fibonacci sequence is the sum of the previous two numbers.
 #   The first two numbers in the sequence are 1 and 1. Compute the 10th
 # #   Fibonacci number.
-FibArray = [0,1]
+FibArray = [0, 1]
+
+
 def fibonacci(n):
     if n < 0:
         print("Incorrect input")
@@ -102,7 +108,9 @@ def fibonacci(n):
 print(fibonacci(10))
 # Write a function to compute the Nth Fibonacci number, where N is a parameter.
 #   You may assume that N will be greater than or equal to 3.
-FibArray = [0,1]
+FibArray = [0, 1]
+
+
 def fibonacci(n):
     if n < 0:
         print("Incorrect input")
@@ -148,7 +156,7 @@ a = 5
 b = 10
 c = 75
 
-if a > b :
+if a > b:
     c = 45
     if b > c:
         a = 25
@@ -163,7 +171,7 @@ print(a, b, c)
 
 d = 55
 e = 72
-f =44
+f = 44
 ans = 0
 if d > e:
     ans = 12
@@ -172,21 +180,21 @@ else:
         ans = 50
     else:
         if f < d * e:
-            ans =100
+            ans = 100
         else:
             ans = 75
 
 print(ans)
 
-def montePi(numDarts):
 
+def montePi(numDarts):
     inCircle = 0
 
     for i in range(numDarts):
         x = random.random()
         y = random.random()
 
-        distance = math.sqrt(x**2 + y**2)
+        distance = math.sqrt(x ** 2 + y ** 2)
 
         if distance <= 1:
             inCircle = inCircle + 1
@@ -194,20 +202,22 @@ def montePi(numDarts):
     pi = inCircle / numDarts * 4
     return pi
 
+
 print(montePi(10000))
 
 import turtle
+
 
 def showMontePi(numDarts):
     scn = turtle.Screen()
     t = turtle.Turtle()
 
-    scn.setworldcoordinates(-2,-2, 2, 2)
+    scn.setworldcoordinates(-1, -1, 1, 1)
 
     t.penup()
     t.goto(-1, 0)
     t.pendown()
-    t.goto(0, 1)
+    t.goto(1, 0)
 
     t.penup()
     t.goto(0, 1)
@@ -221,7 +231,7 @@ def showMontePi(numDarts):
         x = random.random()
         y = random.random()
 
-        distance = math.sqrt(x**2 + y**2)
+        distance = math.sqrt(x ** 2 + y ** 2)
 
         t.goto(x, y)
 
@@ -233,11 +243,12 @@ def showMontePi(numDarts):
 
         t.dot()
 
-    pi = inCircle / numDarts * 4
+    pi = inCircle / numDarts
     scn.exitonclick()
     return pi
 
-showMontePi(1000)
+
+print(showMontePi(1000))
 
 # Your Task:
 # Modify the simulations to plot points in the entire circle. You will have to
